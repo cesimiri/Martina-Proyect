9993af9ec8358dc6b8c65393d197367bb8ffd268
cesimiri <cesimiri@hotmail.com>
2022-12-16 17:52:58 -0500

    se arregla el router para el loginm y registro y se deja registrar

--- src/controllers/nuevoUsuario.js
+++ src/controllers/nuevoUsuario.js
@@ -2,18 +2,17 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { instanciaAxios } from '../Api/Axios';
 
 const nuevoUsuarioController = createAsyncThunk("nuevoUsuario/PostNuevoUsuarioController", async (datosNuevoUsuario) => {
-    const respuesta = await instanciaAxios.post("/", {
-        metodo: "",
+    const respuesta = await instanciaAxios.post("/nuevoUsuario.php", {
+        metodo: "ingresoNU",
         query: datosNuevoUsuario,
     })
-    //   console.log(respuesta.data.estado)
+    //   console.log(respuesta)
+    // console.log("desde el controlador" , datosNuevoUsuario)
 
      if (respuesta.data.estado === 1 ){
         return respuesta.data
     }else{
         // póner
-
-
     }
     
 })
